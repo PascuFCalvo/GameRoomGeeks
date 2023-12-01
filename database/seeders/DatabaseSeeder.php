84a6bf2c3aa4c37ebaab8d417287f87bb3467f66
Vanessa Brito <vanbrigo@MacBook-Air-de-Vanessa.local>
2023-12-01 10:21:46 +0100

    feat: add seeder and factory for users and videogames

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+        ]);
+        \App\Models\User::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
