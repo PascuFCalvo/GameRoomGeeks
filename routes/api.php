79356f6a9237487eef0da74e478078d9b2e93a75
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-12-04 15:48:40 +0100

    commiting local to pull

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VideogameController;
